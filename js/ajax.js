b29ddb64a002f54b7125940dbef369e48b9bac84
Keyboard-l <keyboard-w@outlook.com>
2017-07-02 04:08:12 +0800

    first commit

---
+++ js/ajax.js
@@ -0,0 +1,86 @@
+function createXHR(){
+    var res;
+    if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
+        res = new XMLHttpRequest();
+    }
+    else {  // code for IE6, IE5
+        res = new ActiveXObject("Microsoft.res");
+    }
+    return res;
+}
+
+function get_my_profile()
+{
+    var res = createXHR();
+    res.onreadystatechange = function(){
+        if (res.readyState == 4) {
+            if(res.status == 200) {
+                // 录入信息
+                var data = JSON.parse(res.responseText);
+                document.getElementById('name_and_number').innerHTML = data.name + "<small>" + data.number + "</small>";
+                document.getElementById('email-input3').value = data.email;
+                document.getElementById('phone-input3').value = data.phone;
+                document.getElementById('qq-input3').value = data.qq;
+                document.getElementById('wechat-input3').value = data.wechat;
+                document.getElementById('direction-input3').value = data.direction;
+                document.getElementById('description-input3').value = data.description;
+
+                // 启用并跳转到个人信息页
+                document.getElementById('profile-li').classList.remove('disabled');
+                document.getElementById('profile-a').setAttribute('onclick', "turn_to('profile')");
+                turn_to('profile');
+            }
+            else{
+                // 禁用个人信息页并进入登录页
+                document.getElementById('profile-li').classList.add('disabled');
+                document.getElementById('profile-a').removeAttribute('onclick');
+                turn_to('sign_in');
+            }
+        }
+    };
+    res.open("GET", "../my_profile", true);
+    res.send();
+}
+
+function post_my_profile()
+{
+
+}
+
+function sign_up()
+{
+
+}
+
+function sign_in()
+{
+    var res = createXHR();
+    res.onreadystatechange = function(){
+        if (res.readyState == 4) {
+            if(res.status == 200) {
+                get_my_profile();
+                document.getElementById('pass_wrong').classList.add('hidden');
+                document.getElementById('num_unsigned').classList.add('hidden');
+            }
+            else if(res.status == 401){  // 密码错误
+                document.getElementById('pass_wrong').classList.remove('hidden');
+                document.getElementById('num_unsigned').classList.add('hidden');
+                document.getElementById('profile-li').classList.add('disabled');
+                document.getElementById('profile-a').removeAttribute('onclick');
+                turn_to('sign_in');
+            }
+            else if(res.status == 402){  // 账号不存在
+                document.getElementById('pass_wrong').classList.add('hidden');
+                document.getElementById('num_unsigned').classList.remove('hidden');
+                document.getElementById('profile-li').classList.add('disabled');
+                document.getElementById('profile-a').removeAttribute('onclick');
+                turn_to('sign_in');
+            }
+        }
+    };
+    var number = document.getElementById('number-input2').value;
+    var password = document.getElementById('pass-input2').value;
+    res.open("post", "../sign_in", true);
+    res.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    res.send("number="+number+"&password="+password);
+}
---
+++ js/join_us.js
@@ -0,0 +1,102 @@
+function turn_to(command)
+{
+    if(command=='sign_up'){
+        document.getElementById('sign_up-li').classList.add('active');
+        document.getElementById('sign_in-li').classList.remove('active');
+        document.getElementById('profile-li').classList.remove('active');
+        document.getElementById('sign_up-div').classList.remove('hidden');
+        document.getElementById('sign_in-div').classList.add('hidden');
+        document.getElementById('profile-div').classList.add('hidden');
+    }
+    else if(command=='sign_in'){
+        document.getElementById('sign_up-li').classList.remove('active');
+        document.getElementById('sign_in-li').classList.add('active');
+        document.getElementById('profile-li').classList.remove('active');
+        document.getElementById('sign_up-div').classList.add('hidden');
+        document.getElementById('sign_in-div').classList.remove('hidden');
+        document.getElementById('profile-div').classList.add('hidden');
+
+    }
+    else if(command=='profile'){
+        document.getElementById('sign_up-li').classList.remove('active');
+        document.getElementById('sign_in-li').classList.remove('active');
+        document.getElementById('profile-li').classList.add('active');
+        document.getElementById('sign_up-div').classList.add('hidden');
+        document.getElementById('sign_in-div').classList.add('hidden');
+        document.getElementById('profile-div').classList.remove('hidden');
+    }
+}
+
+function pass_again(command)
+{
+    var pass = document.getElementById('pass-input').value;
+    var pass_again = document.getElementById('pass_again-input').value;
+    if(pass==''){
+        document.getElementById('pass_again-div').className = 'form-group has-error has-feedback';
+        document.getElementById('pass_again-span').className = 'glyphicon glyphicon-remove form-control-feedback';
+    }
+    else if(command=='onfocus'){
+        if(pass!=pass_again){  // 默认的
+            document.getElementById('pass_again-div').className = 'form-group';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+        }
+        else{  // 对的
+            document.getElementById('pass_again-div').className = 'form-group has-success has-feedback';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+            document.getElementById('submit').className = 'btn btn-lg';
+        }
+    }
+    else if(command=='onchange'){
+        if(pass!=pass_again){  // 默认的
+            document.getElementById('pass_again-div').className = 'form-group';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+        }
+        else{  // 对的
+            document.getElementById('pass_again-div').className = 'form-group has-success has-feedback';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+        }
+    }
+    else if(command=='onblur'){
+        if(pass_again==''){  // 默认的
+            document.getElementById('pass_again-div').className = 'form-group';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+        }
+        else if(pass!=pass_again){  // 错的
+            document.getElementById('pass_again-div').className = 'form-group has-error has-feedback';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-remove form-control-feedback';
+        }
+        else{  // 对的
+            document.getElementById('pass_again-div').className = 'form-group has-success has-feedback';
+            document.getElementById('pass_again-span').className = 'glyphicon glyphicon-ok form-control-feedback';
+        }
+    }
+}
+
+function submit_check(formID)
+{
+    if(formID=='form1'){
+        var name1 = document.getElementById('name-input1').value;
+        var number1 = document.getElementById('number-input1').value;
+        var pass1 = document.getElementById('pass-input1').value;
+        var pass_again1 = document.getElementById('pass_again-input1').value;
+        if(name1 != '' && number1 != '' && pass1 != '' && pass1 == pass_again){  // 启用提交按钮
+            document.getElementById('submit1').className = 'btn btn-lg';
+        }
+        else{  // 禁用提交按钮
+            document.getElementById('submit1').className = 'btn btn-lg disabled';
+        }
+    }
+    else if(formID=='form2'){
+        var number2 = document.getElementById('number-input2').value;
+        var pass2 = document.getElementById('pass-input2').value;
+        if(number2 != '' && pass2 != ''){  // 启用提交按钮
+            document.getElementById('submit2').className = 'btn btn-lg';
+        }
+        else{  // 禁用提交按钮
+            document.getElementById('submit2').className = 'btn btn-lg disabled';
+        }
+    }
+    else if(formID=='form3'){
+
+    }
+}
